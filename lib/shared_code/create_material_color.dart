c022de608f9bdc623ac3ba5dc695e9f4255c3208
LeonHoog <75587960+LeonHoog@users.noreply.github.com>
2023-01-23 15:56:30 +0100

    bugfix createMaterialColor

--- lib/shared_code/create_material_color.dart
+++ lib/shared_code/create_material_color.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
 MaterialColor createMaterialColor(Color color) {
+  List strengths = <double>[.05];
   Map<int, Color> swatch = {};
   final int r = color.red, g = color.green, b = color.blue;
 
   for (int i = 1; i < 10; i++) {
-    final double strength = 0.1 * i;
-    final double deltaStrength = 0.5 - strength;
+    strengths.add(0.1 * i);
+  }
+  for (var strength in strengths) {
+    final double ds = 0.5 - strength;
     swatch[(strength * 1000).round()] = Color.fromRGBO(
-      r + ((deltaStrength < 0 ? r : (255 - r)) * deltaStrength).round(),
-      g + ((deltaStrength < 0 ? g : (255 - g)) * deltaStrength).round(),
-      b + ((deltaStrength < 0 ? b : (255 - b)) * deltaStrength).round(),
+      r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+      g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+      b + ((ds < 0 ? b : (255 - b)) * ds).round(),
       1,
     );
   }
