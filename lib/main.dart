f95d893d2590c6a7b5db0f057a0a4c0f34ad9b61
Leon <75587960+LeonHoog@users.noreply.github.com>
2023-06-11 10:32:02 +0200

    Added comments app structure

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,14 @@ import 'package:psalmboek/main_widget/main_widget_base.dart';
 import 'package:psalmboek/providers.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 
+// ---- APP STRUCTURE ----
+// MAIN					            opens Hive database, initializes providers, returns MainWidget
+// MAIN_WIDGET			        sets colors/themes, returns MaterialApp with HomeScreensWrapper as its home
+// HOMESCREENSWRAPPER       contains logic for loading bson, contains button that navigates to SongPageText, returns scaffold with HomeScreen and BookmarksList as body
+//     HOMESCREEN           screen with song selector, contains button that navigates to
+//     BOOKMARKSLIST        screen of created bookmarks
+// SONGPAGE                 screen displaying a selected song
+
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox('settings');
@@ -17,13 +25,13 @@ void main() async {
         ChangeNotifierProvider(create: (_) => SettingsData()),
         ChangeNotifierProvider(create: (_) => DatabaseContentProvider("lib/data/psalmboek1773.bson")),
       ],
-      child: const MyApp(),
+      child: const Main(),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Main extends StatelessWidget {
+  const Main({super.key});
 
   @override
   Widget build(BuildContext context) {
