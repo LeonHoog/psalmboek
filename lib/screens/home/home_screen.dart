9d78ad80ff6ef22570e5995a0578673e0a8b44b4
Leon <75587960+LeonHoog@users.noreply.github.com>
2024-12-27 00:43:17 +0100

    Snappier counter

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -47,53 +47,51 @@ class HomeScreenMobile extends StatelessWidget {
               onChangeStart: (i) {
                 context.read<CounterStates>().setIsAnimatingText(true);
               },
-              onChangeEnd: (i) async {
-                final counterStates = context.read<CounterStates>();
-                if (counterStates.isAnimatingText == true) {
-                  await Future.delayed(const Duration(milliseconds: spinnerDuration ~/ 2));
-                }
-                counterStates.setIsAnimatingText(false);
+              onChangeEnd: (i) {
+                context.read<CounterStates>().setIsAnimatingText(false);
               },
               min: 1,
               max: maxVerse.toDouble(),
               initialValue: value.toDouble(),
               innerWidget: (i) {
-                return context.watch<CounterStates>().isAnimatingText ? AnimatedFlipCounter(
-                  value: value,
-                  fractionDigits: 0,
-                  textStyle: Theme.of(context).textTheme.displayLarge,
-                ) : Center(
-                  child: Padding(
-                    padding: const EdgeInsets.only(bottom: 5),
-                    child: TextField(
-                      textAlignVertical: TextAlignVertical.center,
-                      onSubmitted: (String value) {
-                        int valueInt = int.parse(value);
-                        if (valueInt <= maxVerse && valueInt > 0) {
-                          context.read<CounterStates>().setCounter(valueInt);
-                        }
-                      },
-                      keyboardType: TextInputType.number,
-                      decoration: const InputDecoration(
-                        helperStyle: TextStyle(fontSize: 0),
-                        suffixStyle: TextStyle(fontSize: 0),
-                        enabledBorder: InputBorder.none,
-                        border: InputBorder.none,
+                return context.watch<CounterStates>().isAnimatingText ?
+                  AnimatedFlipCounter(
+                    value: value,
+                    fractionDigits: 0,
+                    textStyle: Theme.of(context).textTheme.displayLarge,
+                  ) :
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 5),
+                      child: TextField(
+                        textAlignVertical: TextAlignVertical.center,
+                        onSubmitted: (String value) {
+                          int valueInt = int.parse(value);
+                          if (valueInt <= maxVerse && valueInt > 0) {
+                            context.read<CounterStates>().setCounter(valueInt);
+                          }
+                        },
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                          helperStyle: TextStyle(fontSize: 0),
+                          suffixStyle: TextStyle(fontSize: 0),
+                          enabledBorder: InputBorder.none,
+                          border: InputBorder.none,
+                        ),
+                        maxLength: 4,
+                        maxLines: 1,
+                        controller: TextEditingController(text: context.watch<CounterStates>().count.toString()),
+                        style: TextStyle(
+                          fontFamily: Theme.of(context).textTheme.displayLarge!.fontFamily,
+                          fontSize: Theme.of(context).textTheme.displayLarge!.fontSize,
+                          fontWeight: Theme.of(context).textTheme.displayLarge!.fontWeight,
+                          fontStyle: Theme.of(context).textTheme.displayLarge!.fontStyle,
+                          color: Theme.of(context).textTheme.displayLarge!.color,
+                        ),
+                        textAlign: TextAlign.center,
                       ),
-                      maxLength: 4,
-                      maxLines: 1,
-                      controller: TextEditingController(text: context.read<CounterStates>().count.toString()),
-                      style: TextStyle(
-                        fontFamily: Theme.of(context).textTheme.displayLarge!.fontFamily,
-                        fontSize: Theme.of(context).textTheme.displayLarge!.fontSize,
-                        fontWeight: Theme.of(context).textTheme.displayLarge!.fontWeight,
-                        fontStyle: Theme.of(context).textTheme.displayLarge!.fontStyle,
-                        color: Theme.of(context).textTheme.displayLarge!.color,
-                      ),
-                      textAlign: TextAlign.center,
                     ),
-                  ),
-                );
+                  );
               },
             ),
           ),
